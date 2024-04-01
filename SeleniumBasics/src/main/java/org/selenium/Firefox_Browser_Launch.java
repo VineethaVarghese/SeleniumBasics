9b9ded548340448cfa9a52a0aa2f7f9d8735e7bb
shon4 <shon4@DESKTOP-K6JQS20>
2024-04-01 13:27:49 +0530

    Commit Selenium Basics & Push

---
+++ SeleniumBasics/src/main/java/org/selenium/Firefox_Browser_Launch.java
@@ -0,0 +1,37 @@
+package org.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Firefox_Browser_Launch {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+driver.get("https://demowebshop.tricentis.com/");
+		
+		//to maximize the url 
+		driver.manage().window().maximize();
+		
+		//to get the Title of URL, its a string type ,so storing in a variable
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		//to get current URL
+		String current_url = driver.getCurrentUrl();
+		System.out.println(current_url);
+		
+		//to get window handle id
+		String window_handle = driver.getWindowHandle();
+		System.out.println(window_handle);
+		
+		//to get the html code of the current page
+		String page_source = driver.getPageSource();
+		System.out.println(page_source);
+		
+		//to close the window
+		driver.close();
+
+	}
+
+}
